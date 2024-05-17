c65f1e62cef810c6857e9ee498b21a52ea5f041c
dbugentity19 <anayaray19@gmail.com>
2024-05-17 13:27:16 +0500

    feat: added editable location text input

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import { useState } from 'react';
 import './App.css';
+import Input from './components/Input';
 import Navbar from './components/Navbar';
 
 function App() {
+  const [location, setLocation] = useState('');
+
   return (
     <div className="border-[15px] border-pink-300 min-h-screen">
       {/* Navbar */}
       <Navbar />
+      <div className="border-b-[3px] border-black pb-2 mt-4 max-w-[800px] mx-auto">
+        {/* Input */}
+        <Input
+          name={'location'}
+          type={'text'}
+          value={location}
+          placeholder={'Dolors Park, SF'}
+          onChange={(e) => setLocation(e.target.value)}
+        />
+      </div>
     </div>
   );
 }
---
+++ src/components/Input.js
@@ -0,0 +1,22 @@
+import { MapPinIcon } from '@heroicons/react/20/solid';
+
+const Input = ({ name, type, value, placeholder, onChange }) => {
+  return (
+    <div>
+      <div className="relative rounded-md shadow-sm">
+        <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+          <MapPinIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+        </div>
+        <input
+          type={type}
+          name={name}
+          value={value}
+          className="block w-full rounded-md border-0 py-1.5 pl-10 text-gray-900 ring-0  placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+          onChange={onChange}
+          placeholder={placeholder}
+        />
+      </div>
+    </div>
+  );
+};
+export default Input;
